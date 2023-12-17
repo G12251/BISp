900ed9800605a1089eb4ecb8126be456ab19778d
G12251 <gulnozasaparmatova@gmail.com>
2023-12-17 16:29:09 +0500

    Front end setup

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from 'antd';
+
+function Register() {
+  return <div className="flex justify-center h-screen items-center">
+        <Button type="primary">Register</Button>
+
+  </div>;
+}
+
+export default Register;
+
